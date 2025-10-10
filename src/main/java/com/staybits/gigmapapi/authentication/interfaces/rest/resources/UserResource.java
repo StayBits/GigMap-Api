c0d74e3d87c1e265ab7780040339e5bf15b0e84b
D4D3v4l <diegocollantes202010@gmail.com>
2025-10-10 16:43:06 -0500

    fix(Backend): Se corrigió el UserResource

--- src/main/java/com/staybits/gigmapapi/authentication/interfaces/rest/resources/UserResource.java
+++ src/main/java/com/staybits/gigmapapi/authentication/interfaces/rest/resources/UserResource.java
@@ -1,4 +1,4 @@
 package com.staybits.gigmapapi.authentication.interfaces.rest.resources;
 
-public record UserResource(Long id, String email, String username, String name, String role) {
+public record UserResource(Long id, String email, String username, String name, String role, String image) {
 }
